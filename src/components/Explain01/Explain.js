6ae835a334c8e2f125150b9ff051aba4baae4666
chiba <ryotai2677@gmail.com>
2024-08-04 17:16:05 +0900

    aaa

--- src/components/Explain01/Explain.js
+++ src/components/Explain01/Explain.js
@@ -5,16 +5,19 @@ const Explain = () => {
     return (
         <section className={styles.Section}>
             <div className={styles.Grid}>
-                <h3>Solve the problem</h3>
-                <h2>様々な課題解決サポートを<br/>ご用意しています</h2>
-                <p>
+                <div className={styles.Title}>
+                    <span/>
+                    <h3>Solve the problem</h3>
+                    <h2>様々な課題解決サポートを<br/>ご用意しています</h2>
+                </div>
+                <p className={styles.Text}>
                     ホームページ制作とWebマーケティングのノウハウをもとに、
                     飲食店様に特化した課題解決サポートをご提供しています。
                     新規顧客の獲得からリピーターの獲得まで、お店の集客にお困りの方はお気軽にご相談ください。
                     お客様の売り上げアップに貢献できる新しい価値を創造し、
                     妥協をしない成果物を納品することをお約束いたします。
                 </p>
-                <div className={styles.textCards}>
+                <div className={styles.TextCards}>
                     <p>新しくホームページを立ち上げたい</p>
                     <p>新しくホームページを立ち上げたい</p>
                     <p>新しくホームページを立ち上げたい</p>
